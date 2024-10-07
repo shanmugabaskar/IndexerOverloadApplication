040c9cda068124f3b4194e41456036d7f01fd9f9
shanmugabaskar <baskar.shan@gmail.com>
2024-10-07 09:30:50 +0530

    Add project files.

---
+++ IndexerOverloadApplication/IndexedNames.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerOverloadApplication
+{
+    internal class IndexedNames
+    {
+        private string[] namelist = new string[size];
+        static public int size = 10;
+
+        public IndexedNames()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                namelist[i] = "N. A.";
+            }
+        }
+        public string this[int index]
+        {
+            get
+            {
+                string tmp;
+
+                if (index >= 0 && index <= size - 1)
+                {
+                    tmp = namelist[index];
+                }
+                else
+                {
+                    tmp = "";
+                }
+
+                return (tmp);
+            }
+            set
+            {
+                if (index >= 0 && index <= size - 1)
+                {
+                    namelist[index] = value;
+                }
+            }
+        }
+
+        public int this[string name]
+        {
+            get
+            {
+                int index = 0;
+
+                while (index < size)
+                {
+                    if (namelist[index] == name)
+                    {
+                        return index;
+                    }
+                    index++;
+                }
+                return index;
+            }
+        }
+    }
+}
---
+++ IndexerOverloadApplication/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using IndexerOverloadApplication;
+
+IndexedNames names = new IndexedNames();
+names[0] = "Zara";
+names[1] = "Riz";
+names[2] = "Nuha";
+names[3] = "Asif";
+names[4] = "Davinder";
+names[5] = "Sunil";
+names[6] = "Rubic";
+
+//using the first indexer with int parameter
+for (int i = 0; i < IndexedNames.size; i++)
+{
+    Console.WriteLine(names[i]);
+}
+
+//using the second indexer with the string parameter
+Console.WriteLine(names["Nuha"]);
+Console.ReadKey();
